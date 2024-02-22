11ee4fd1447904a2bd860b3dbd691b67f1e00084
AbineshPrami1202 <abinesh3292@gmail.com>
2024-02-22 17:20:05 +0530

    ok

---
+++ src/org/database/Student.java
@@ -0,0 +1,23 @@
+package org.database;
+
+public class Student {
+	public static void main(String[] args) {
+		boolean age = true;
+		System.out.println("\nStu age:"+ age);
+		short fees = 4000;
+		System.out.println("\nStu Fees:"+4000);
+		int average = 12345;
+		System.out.println("\nStu Average:"+average);
+		double Phnum = 12.12345678234567;
+		System.out.println("\nStud Ph no:"+Phnum);
+		char gender = '@';
+		System.out.println("\nStu Gender:"+gender);
+		String Name = "Ararttyuturyj@123";
+		System.out.println("\nStud Name:"+Name);
+		
+		
+		
+		
+	}
+
+}
